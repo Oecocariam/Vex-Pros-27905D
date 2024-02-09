9589ec170ea6cbc38ac3f587fa57591c2766942d
Oeceacarium <benfart22@gmail.com>
2024-02-09 10:55:50 -0600

    motor encoder in degrees

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,15 @@
 
 		pros::Controller master (CONTROLLER_MASTER);
 
-	pros::Motor left1 (1, MOTOR_GEARSET_18, true);
-	pros::Motor left2 (2, MOTOR_GEARSET_18, false); 
-	pros::Motor right1 (9, MOTOR_GEARSET_18, false); 
-	pros::Motor right2 (10, MOTOR_GEARSET_18, false); 
+	pros::Motor left1 ( (int)1, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+	pros::Motor left2 (2, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES); 
+	pros::Motor right1 (9, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES); 
+	pros::Motor right2 (10, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES); 
 
-	pros::Motor wingLeft (7, MOTOR_GEARSET_36, false);
-	pros::Motor wingRight (8, MOTOR_GEARSET_36, true);
+	pros::Motor wingLeft (7, MOTOR_GEARSET_36, false, MOTOR_ENCODER_DEGREES);
+	pros::Motor wingRight (8, MOTOR_GEARSET_36, true, MOTOR_ENCODER_DEGREES);
 
-	pros::Motor launcher (11, MOTOR_GEARSET_36, false);
-	
+	pros::Motor launcher (11, MOTOR_GEARSET_36, false, MOTOR_ENCODER_DEGREES);
 
 
 double averageMotorVoltage(){
